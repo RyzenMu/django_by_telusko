d9dce3b6e51202d9e6a938a4b1298f0177358176
RyenMu <muruga13in2022@gmail.com>
2022-11-26 18:37:02 +0530

    static folder added

--- sutrula/app_sutrula/views.py
+++ sutrula/app_sutrula/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'home_page.html')
+    return render(request, 'index.html')
 
 def makemytrip(request):
     return render(request, 'makemytrip.html')
