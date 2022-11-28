3109125b77efe7f9a610217d06049509b74a3f7a
RyenMu <muruga13in2022@gmail.com>
2022-11-28 22:08:23 +0530

    colorlib-1 deleted

--- colorlib_1/app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-locations = ['ambur', 'bangalore', 'chennai', 'delhi', 'erode', 'faridabad']
-
--- colorlib_1/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from django.http import HttpResponse
-from . import views
-
-urlpatterns = [ 
-    path('', views.index, name='Home Page'),
-    path('index.html', views.listings, name='listings'),
-    path('contact.html', views.contact, name='contact')
-]
