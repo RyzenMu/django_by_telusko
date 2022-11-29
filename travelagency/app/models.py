88eb696dca448284b590d01043d42733107ae0c9
RyenMu <muruga13in2022@gmail.com>
2022-11-30 04:41:56 +0530

    checking for loop

--- travelagency/app/models.py
+++ travelagency/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Destinations:
+    id : int
+    name : str 
+    img : str 
+    des : str 
+    price : int 
+    
--- travelagency/app/views.py
+++ travelagency/app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destinations
 
 # Create your views here.
 
 def home(request):
     loaction_1 = 'Mumbai'
     location_2 = 'Goa'
-    return render(request, 'index.html', {'location_1': loaction_1, 'location_2' : location_2})
+
+    dest1 = Destinations()
+    dest1.name = 'mumbai'
+    dest1.price = 700
+
+    dest2 = Destinations()
+    dest2.name = 'Goa'
+    dest2.price = 800
+
+
+    # return render(request, 'index.html', {'location_1': loaction_1, 'location_2' : location_2,
+    # 'price_1' : 700, 'price_2' : 800})
+
+def forloop(request):
+    names = ['apple', 'banana', 'carrot']
+    return render(request, 'forloop.html', {'names' : names})
