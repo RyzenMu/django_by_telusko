202730ddd335fe20d3af4d5320b528f133d2d798
RyenMu <muruga13in2022@gmail.com>
2022-12-06 21:01:29 +0530

    database migrations

--- dynamic_page/app/models.py
+++ dynamic_page/app/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Destinations:
-    name: str
-    price: int 
-    image : str
+class Destinations(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField
+    image = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    boole = models.BooleanField(default=False)
